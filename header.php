1a0c905dac4846d60341cba5a125822ddfc0840f
Hal Gatewood <hal@halgatewood.com>
2013-04-04 15:25:57 -0500

    If statement for iPhone Nav

--- header.php
+++ header.php
@@ -18,8 +18,11 @@
 	
 	<script>
 		// HIDE NAV BAR ON IPHONE
-		addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
-		function hideURLbar(){ window.scrollTo(0,1);}
+		if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) 
+		{
+			addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+			function hideURLbar(){ window.scrollTo(0,1);}
+		}
 	</script>
 		
 </head>
